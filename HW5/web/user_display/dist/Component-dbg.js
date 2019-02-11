2dfd6a7192cc0d95cae7fa939cc618337f1ae25d
Maksim Zhytkevich <maksim.zhytkevich@leverx.com>
2019-02-11 18:59:50 +0300

    HW5

---
+++ HW5/web/user_display/dist/Component-dbg.js
@@ -0,0 +1,24 @@
+sap.ui.define([
+	"sap/ui/core/UIComponent",
+	"sap/ui/Device",
+], function (UIComponent, Device) {
+	"use strict";
+
+	return UIComponent.extend("user_display.Component", {
+
+		metadata: {
+			manifest: "json"
+		},
+
+		/**
+		 * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+		 * @public
+		 * @override
+		 */
+		init: function () {
+			// call the base component's init function
+			console.log("component init");
+			UIComponent.prototype.init.apply(this, arguments);
+		}
+	});
+});
---
+++ HW5/web/user_display/dist/controller/user_display.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("user_display.controller.user_display", {
+        onInit: function () {
+			console.log("controller init");
+		}
+     });
+
+});
