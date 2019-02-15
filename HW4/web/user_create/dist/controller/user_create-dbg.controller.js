219d20270ea77bbcd866d0af8478bfbbca767391
Maksim Zhytkevich <maksim.zhytkevich@leverx.com>
2019-02-15 14:53:57 +0300

    HW4 user_create post users

--- HW4/web/user_create/dist/controller/user_create-dbg.controller.js
+++ HW4/web/user_create/dist/controller/user_create-dbg.controller.js
@@ -6,6 +6,26 @@ sap.ui.define([
 	return Controller.extend("user_create.controller.user_create", {
         onInit: function () {
 			console.log("controller init");
+		},
+		createUser: function () {
+			var Name = sap.ui.getCore().byId(this.getView().sId + "--input_name").getValue();
+			console.log(Name);
+
+			var settings = {
+				"async": true,
+				"crossDomain": true,
+				"url": "https://p2001081134trial-maksimzhytkevich-space1-router.cfapps.eu10.hana.ondemand.com/api/xsodata/himta.xsodata/Users",
+				"method": "POST",
+				"headers": {
+					"content-type": "application/json"
+				},
+				"processData": false,
+				"data": "{\"name\": \"" + Name  + "\"}"
+			};
+
+			$.ajax(settings).done(function (response) {
+				console.log(response);
+			});
 		}
      });
 
